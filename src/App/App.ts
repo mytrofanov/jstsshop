953d18d69dcaf13ec1768f6d85d00c7ae6565f49
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-31 11:59:11 +0300

    ProductsList.ts

--- src/App/App.ts
+++ src/App/App.ts
@@ -1,5 +1,13 @@
+import { ProductsList } from './Components/ProductsList';
+
 export class App {
+  private productList = new ProductsList();
+
   render() {
-    return '<h1>App</h1>';
+    return `
+        <h1>App</h1>
+        <div>${this.productList.render()}</div>
+    
+    `;
   }
 }
