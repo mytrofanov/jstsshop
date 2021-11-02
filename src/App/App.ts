e51e13e71ea140119dc1e1c70fa842b06bbf9749
mytrofanov <mytrofanovmaxym@gmail.com>
2021-11-02 11:38:05 +0300

    Cart beginning

--- src/App/App.ts
+++ src/App/App.ts
@@ -1,13 +1,18 @@
 import { ProductsList } from './Components/ProductsList';
+import { Cart } from './Components/Cart';
 
 export class App {
   private productList = new ProductsList();
+  private cart = new Cart();
 
   render() {
     return `
          <div class="container">
             <div class="row">
-                <div class="col-12">
+                <div class="col-3">
+         ${this.cart.render()}
+                </div>
+                <div class="col-9">
          ${this.productList.render()} 
                 </div>
                </div>
---
+++ src/App/Components/Cart/Cart.ts
@@ -0,0 +1,18 @@
+export class Cart {
+  render() {
+    return `
+<div>
+       <ul class="list-group" style="margin-top: 1em">
+  <li class="list-group-item d-flex justify-content-between align-items-start">An item
+   <button type="button" class="btn btn-outline"> + </button>
+    <button type="button" class="btn btn-outline"> - </button>
+   <span class="badge bg-primary rounded-pill">14</span>
+  </li>  
+</ul>
+<p>
+Summary: 3 items, $1220 
+</p>
+</div>
+`;
+  }
+}
